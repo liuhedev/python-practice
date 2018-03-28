ed4e006422e9158e80e98f1e14050401eda30998
liuhe <liuhe@juran.com.cn>
2018-03-29 01:12:38 +0800

    if-elif-else

--- day1/if_3.py
+++ day1/if_3.py
@@ -2,10 +2,12 @@
 age = input("请输入您的年龄:")
 try:
     age_num = int(age)
-    if age_num > 18 :
-        print("已经成年，可以去网吧了")
+    if age_num < 18  :
+        print("未成年，去玩泥巴。")
+    elif age_num > 80:
+        print("大爷，注意身体，游戏伤身")
     else:
-        print("未成年，去玩泥巴")
+        print("已经成年了，可以去网吧了。")
 except:
     print("请输入正确的年龄!!!!")
 
