647323e8725e21372c27a4d9495be08c35ecf7d7
liuhe <liuhe@juran.com.cn>
2018-03-29 22:30:52 +0800

    while

---
+++ day2/and_2.py
@@ -0,0 +1,10 @@
+u = input("你去吗？")
+uWife = input("你媳妇去吗？")
+if u =="去" and uWife == "去":
+    print("可以办成某件")
+elif u == "去" or uWife == "去":
+    print("或许办不成")
+else :
+    print("说啥来着？")
+print("判断结束")
+    #前边有空格，报错！ print("是真的")
---
+++ day2/print_1.py
@@ -0,0 +1,4 @@
+name = "liuhe"
+age = 20
+address = "山东临沂"
+print("姓名：%s,年龄：%d, 地址：%s"%(name,age,address))
